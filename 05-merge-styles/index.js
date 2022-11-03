34dd60aca04b12c98132d243880d3af85a47255f
Andkhiz <andkhiz@gmail.com>
2022-11-03 16:27:22 +0200

    feat: change import into 05-merge-styles to CommnonJS and add possibility import as module

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,27 +1,32 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-import { open, readdir, readFile, appendFile } from 'node:fs/promises';
+//import path from 'path';
+//import {fileURLToPath} from 'url';
+//import { open, readdir, readFile, appendFile } from 'node:fs/promises';
+const path = require("path");
+const { open, readdir, readFile, appendFile } = require("node:fs/promises");
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 
-const dirNameStyle = path.join(__dirname, "styles");
-const dirNamePaste = path.join(__dirname, "project-dist");
-const fileStyleName = path.join(dirNamePaste, "bundle.css");
+const dirNameStyleDefault = path.join(__dirname, "styles");
+const fileStyleNameDefault = path.join(__dirname, "project-dist", "bundle.css");
 
-try {
-  await open(fileStyleName, "w");
-  const files = await readdir(dirNameStyle, {withFileTypes: true});
-  for ( let file of files ) {
-    if ( file.isFile() ) {
-      if ( path.extname(file.name) === '.css' ) {
-        let contentFileStyle = await readFile( path.join(dirNameStyle, file.name) , { encoding: 'utf8' });
-        await appendFile(fileStyleName, contentFileStyle + "\n");
+async function mergeStyle(dirNameStyle = dirNameStyleDefault, fileStyleName = fileStyleNameDefault ) {
+  try {
+    await open(fileStyleName, "w");
+    const files = await readdir(dirNameStyle, {withFileTypes: true});
+    for ( let file of files ) {
+      if ( file.isFile() ) {
+        if ( path.extname(file.name) === '.css' ) {
+          let contentFileStyle = await readFile( path.join(dirNameStyle, file.name) , { encoding: 'utf8' });
+          await appendFile(fileStyleName, contentFileStyle + "\n");
+        }
       }
     }
+  } catch (err) {
+    console.error(err.message);
   }
-} catch (err) {
-  console.error(err.message);
 }
 
-
+if ( module.parent )
+  module.exports = mergeStyle;
+else mergeStyle();
