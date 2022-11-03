82fa4668b4901d46d1e261e591e4301de87d9761
Andkhiz <andkhiz@gmail.com>
2022-11-03 15:27:05 +0200

    feat: shange import on CommonJS and posibility to import module

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,16 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
-import { mkdir, readdir, copyFile, unlink } from 'node:fs/promises';
+//import path from 'path';
+//import {fileURLToPath} from 'url';
+//import { mkdir, readdir, copyFile, unlink } from 'node:fs/promises';
+const path = require("path");
+const { mkdir, readdir, copyFile, unlink, rmdir } = require('node:fs/promises');
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+//const __filename = fileURLToPath(import.meta.url);
+//const __dirname = path.dirname(__filename);
 
-var dirNameCopy = path.join(__dirname, 'files');
-var dirNamePaste = path.join(__dirname, 'files-copy');
+const dirNameCopyDefault = path.join(__dirname, 'files');
+const dirNamePasteDefault = path.join(__dirname, 'files-copy');
 
+async function copyDirectory(dirNameCopy = dirNameCopyDefault, dirNamePaste = dirNamePasteDefault, onlyFiles = true ) {
 try {
   const createDir = await mkdir(dirNamePaste, { recursive: true });
   const files = await readdir(dirNameCopy, {withFileTypes: true});
@@ -19,15 +22,25 @@ try {
       await copyFile(fileNameCopy, fileNamePaste);
       filesName.push(file.name);
     }
+    else {
+      if (onlyFiles === false) {
+        await mkdir( path.join(dirNamePaste, file.name), { recursive: true });
+        await copyDirectory( path.join(dirNameCopy, file.name), path.join(dirNamePaste, file.name), false );
+        filesName.push(file.name);
+      }
+    }
   }
   if (createDir === undefined ) {
     const filesCopy = await readdir(dirNamePaste, {withFileTypes: true});
     for (let file of filesCopy) {
       if (file.isFile() ) {
         if ( !filesName.includes(file.name) ) {
-          let delFile = await unlink(path.join(dirNamePaste, file.name));
+          await unlink(path.join(dirNamePaste, file.name));
         }
-      }
+      }else 
+      if (!filesName.includes(file.name)) {
+        await rmdir(path.join(dirNamePaste, file.name));
+      } 
     }
     
 //  console.log(`created ${createDir}`);
@@ -35,5 +48,8 @@ try {
 } catch (err) {
   console.error(err.message);
 }
+}
 
-
+if ( module.parent )
+  module.exports = copyDirectory ;
+else copyDirectory();
